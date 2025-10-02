ac3af66ebf5befd7631e5ba4bf1e4e626afd1293
Kuldeep Mandal <kuldeepmandal175514@gmail.com>
2025-10-02 13:42:57 +0530

    Second push and commit

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,4 +1,3 @@
-
 import {Link} from "react-router";
 import ScoreCircle from "~/components/ScoreCircle";
 import {useEffect, useState} from "react";
--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,13 +1,13 @@
-import Navbar from "~/components/Navbar";
 import {type FormEvent, useState} from 'react'
+import Navbar from "~/components/Navbar";
 import FileUploader from "~/components/FileUploader";
-import {convertPdfToImage} from "~/lib/pdf2img";
 import {usePuterStore} from "~/lib/puter";
 import {useNavigate} from "react-router";
+import {convertPdfToImage} from "~/lib/pdf2img";
 import {generateUUID} from "~/lib/utils";
 import {prepareInstructions} from "../../constants";
 
-const Upload= () => {
+const Upload = () => {
     const { auth, isLoading, fs, ai, kv } = usePuterStore();
     const navigate = useNavigate();
     const [isProcessing, setIsProcessing] = useState(false);
@@ -18,8 +18,9 @@ const Upload= () => {
         setFile(file)
     }
 
-    const handleAnalyze = async ({companyName, jobTitle, jobDescription, file}:{ companyName: string, jobTitle: string, jobDescription: string, file: File }) => {
+    const handleAnalyze = async ({ companyName, jobTitle, jobDescription, file }: { companyName: string, jobTitle: string, jobDescription: string, file: File  }) => {
         setIsProcessing(true);
+
         setStatusText('Uploading the file...');
         const uploadedFile = await fs.upload([file]);
         if(!uploadedFile) return setStatusText('Error: Failed to upload file');
@@ -47,7 +48,7 @@ const Upload= () => {
 
         const feedback = await ai.feedback(
             uploadedFile.path,
-            prepareInstructions({jobTitle, jobDescription})
+            prepareInstructions({ jobTitle, jobDescription })
         )
         if (!feedback) return setStatusText('Error: Failed to analyze resume');
 
@@ -59,14 +60,13 @@ const Upload= () => {
         await kv.set(`resume:${uuid}`, JSON.stringify(data));
         setStatusText('Analysis complete, redirecting...');
         console.log(data);
-        // navigate(`/resume/${uuid}`);
-
-
+        navigate(`/resume/${uuid}`);
     }
+
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        const form:any = e.currentTarget.closest('form');
-        if (!form) return;
+        const form = e.currentTarget.closest('form');
+        if(!form) return;
         const formData = new FormData(form);
 
         const companyName = formData.get('company-name') as string;
@@ -74,9 +74,9 @@ const Upload= () => {
         const jobDescription = formData.get('job-description') as string;
 
         if(!file) return;
-        handleAnalyze({ companyName, jobTitle, jobDescription, file});
-    }
 
+        handleAnalyze({ companyName, jobTitle, jobDescription, file });
+    }
 
     return (
         <main className="bg-[url('/images/bg-main.svg')] bg-cover">
@@ -91,7 +91,7 @@ const Upload= () => {
                             <img src="/images/resume-scan.gif" className="w-full" />
                         </>
                     ) : (
-                        <h2>Drop your resume for an ATS check and Improvements</h2>
+                        <h2>Drop your resume for an ATS score and improvement tips</h2>
                     )}
                     {!isProcessing && (
                         <form id="upload-form" onSubmit={handleSubmit} className="flex flex-col gap-4 mt-8">
@@ -107,22 +107,20 @@ const Upload= () => {
                                 <label htmlFor="job-description">Job Description</label>
                                 <textarea rows={5} name="job-description" placeholder="Job Description" id="job-description" />
                             </div>
+
                             <div className="form-div">
-                                <label htmlFor="uploader">Uploader</label>
+                                <label htmlFor="uploader">Upload Resume</label>
                                 <FileUploader onFileSelect={handleFileSelect} />
                             </div>
 
-                            <button className="primary-button" type="submit" >
+                            <button className="primary-button" type="submit">
                                 Analyze Resume
                             </button>
-
-
                         </form>
                     )}
                 </div>
             </section>
         </main>
-
     )
 }
 export default Upload
